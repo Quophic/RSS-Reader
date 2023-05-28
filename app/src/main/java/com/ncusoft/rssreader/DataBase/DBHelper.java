359190a0e526cfb99dbc9337886a282172018831
quophic <quophic@qq.com>
2023-05-29 05:36:58 +0800

    更改数据库表

--- app/src/main/java/com/ncusoft/rssreader/DataBase/DBHelper.java
+++ app/src/main/java/com/ncusoft/rssreader/DataBase/DBHelper.java
@@ -8,22 +8,24 @@ import com.ncusoft.rssreader.DataBase.Contract.RSSItemsContract;
 import com.ncusoft.rssreader.DataBase.Contract.RSSSourcesContract;
 
 public class DBHelper extends SQLiteOpenHelper {
-    public static final int DATABASE_VERSION = 3;
+    public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "rss.db";
     private static final String SQL_CREATE_TITLES =
             "CREATE TABLE " + RSSSourcesContract.TABLE_NAME + "(" +
                     RSSSourcesContract._ID + " INTEGER PRIMARY KEY," +
                     RSSSourcesContract.TITLE + " TEXT," +
-                    RSSSourcesContract.LINK + " TEXT," +
+                    RSSSourcesContract.LINK + " TEXT UNIQUE," +
                     RSSSourcesContract.IMAGE + " TEXT)";
     private static final String SQL_CREATE_RSS_ITEM =
             "CREATE TABLE " + RSSItemsContract.TABLE_NAME + "(" +
                     RSSItemsContract._ID + " INTEGER PRIMARY KEY," +
                     RSSItemsContract.SOURCE_ID + "INTEGER," +
                     RSSItemsContract.TITLE + " TEXT," +
-                    RSSItemsContract.LINK + " TEXT," +
+                    RSSItemsContract.LINK + " TEXT UNIQUE," +
                     RSSItemsContract.PUB_DATE + " INTEGER," +
-                    RSSItemsContract.STATUS + " INTEGER)";
+                    RSSItemsContract.STATUS + " INTEGER," +
+                    "FOREIGN KEY (" + RSSItemsContract.SOURCE_ID + " )" +
+                    "REFERENCES " + RSSSourcesContract.TABLE_NAME + "(" + RSSSourcesContract._ID +"))";
     public DBHelper(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -35,15 +37,5 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        switch(oldVersion){
-            case 1:
-                String ADD_TYPE = "ALTER TABLE " + RSSSourcesContract.TABLE_NAME +
-                        " ADD " + RSSSourcesContract.IMAGE + " TEXT";
-                db.execSQL(ADD_TYPE);
-                if(newVersion <= 2) break;
-            case 2:
-                db.execSQL(SQL_CREATE_RSS_ITEM);
-                if (newVersion <= 3) break;
-        }
     }
 }
