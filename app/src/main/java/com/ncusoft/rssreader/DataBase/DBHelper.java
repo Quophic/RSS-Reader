8672b88751735b0dc4e3cd419053723349b25eb2
quophic <quophic@qq.com>
2023-05-28 14:13:41 +0800

    添加标题图片字段

--- app/src/main/java/com/ncusoft/rssreader/DataBase/DBHelper.java
+++ app/src/main/java/com/ncusoft/rssreader/DataBase/DBHelper.java
@@ -5,13 +5,14 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
     public static final String DATABASE_NAME = "rss.db";
     private static final String SQL_CREATE_RECORD =
             "CREATE TABLE " +TableContract.TABLE_NAME + "(" +
                     TableContract._ID + " INTEGER PRIMARY KEY," +
                     TableContract.TITLE + " TEXT," +
-                    TableContract.LINK + " TEXT)";
+                    TableContract.LINK + " TEXT," +
+                    TableContract.IMAGE + " TEXT)";
     public DBHelper(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -22,6 +23,13 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        switch(oldVersion){
+            case 1:
+                String ADD_TYPE = "ALTER TABLE " + TableContract.TABLE_NAME +
+                        " ADD " +TableContract.IMAGE + " TEXT";
+                db.execSQL(ADD_TYPE);
+                if(newVersion <= 2) break;
+            case 2:
+        }
     }
 }
--- app/src/main/java/com/ncusoft/rssreader/DataBase/TableContract.java
+++ app/src/main/java/com/ncusoft/rssreader/DataBase/TableContract.java
@@ -6,4 +6,5 @@ public final class TableContract implements BaseColumns {
     public static final String TABLE_NAME = "subscribed_rss";
     public static final String TITLE = "title";
     public static final String LINK = "link";
+    public static final String IMAGE = "image";
 }
