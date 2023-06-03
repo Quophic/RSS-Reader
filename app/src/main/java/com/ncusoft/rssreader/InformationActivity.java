262293ddc7a808b5a54ea50c80ae1aab4ab2f2a1
quophic <quophic@qq.com>
2023-06-03 10:37:45 +0800

    修改软件信息页面

--- app/src/main/java/com/ncusoft/rssreader/InformationActivity.java
+++ app/src/main/java/com/ncusoft/rssreader/InformationActivity.java
@@ -4,17 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-public class DetailActivity extends AppCompatActivity {
+public class InformationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.activity_information);
         ActionBar actionBar = getSupportActionBar();
         if(actionBar != null){
             actionBar.setDisplayHomeAsUpEnabled(true);
